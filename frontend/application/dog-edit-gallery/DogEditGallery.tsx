92928fd354eef4649f083cc6d369d5f2a8e69f27
darqwski <dariusz.cabala.pl@gmail.com>
2022-06-15 17:26:03 +0200

    [DOGGO] - Managing gallery

--- frontend/application/dog-edit-gallery/DogEditGallery.tsx
+++ frontend/application/dog-edit-gallery/DogEditGallery.tsx
@@ -1,33 +1,67 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import BasicPage from '../../components/basic-page/BasicPage'
-import {Button} from "react-materialize";
-import appRequest from "../../utils/app-request";
-import {useParams} from "react-router-dom";
-import {uploadFile} from "../../utils/upload-file";
-
-export interface IDogGalleryPage {}
+import { Button, Card } from 'react-materialize'
+import appRequest from '../../utils/app-request'
+import { useParams } from 'react-router-dom'
+import { uploadFile } from '../../utils/upload-file'
+import useAppRequest from '../../hooks/use-app-request'
+import './dog-edit-gallery.less'
+import { SnackbarContext } from '../../components/snackbar/snackbar-context'
 
 const DogEditGallery: React.FC = () => {
     const { dogId } = useParams<{ dogId: string }>()
+    const { addSnackBar } = useContext(SnackbarContext)
+    const { data: dogImages, refresh } = useAppRequest<
+        { fileUrl: string; fileId: number }[]
+    >({ url: ` /API/dog/${dogId}/images` })
 
-    const uploadImage:  React.ChangeEventHandler<HTMLInputElement> =async (event) => {
+    const uploadImage: React.ChangeEventHandler<HTMLInputElement> = async (
+        event
+    ) => {
         const { files } = event.target
 
-        if(!files || !files.length){
-            return;
+        if (!files || !files.length) {
+            return
         }
 
-       await uploadFile('/API/dog/'+dogId+'/add-photo', files?.[0] )
+        await uploadFile('/API/dog/' + dogId + '/add-photo', files?.[0])
+    }
+
+    const removeFile = async (fileId: number) => {
+        await appRequest({ method: 'DELETE', url: '/API/dog/remove-image/' + fileId })
+        addSnackBar({ text: 'Usunięto obrazek' })
+        refresh()
     }
 
     return (
         <BasicPage>
             <div>
-                <form action="/API/dog/add-photo" method="POST" >
-                    <label htmlFor="file">Dodaj zdjęcie</label>
-                    <input type="file" id="file" onChange={uploadImage} />
-                    <button type="submit">SUBMIT </button>
-                </form>
+                <Card>
+                    <h3>Dodaj zdjęcie</h3>
+                    <div>
+                        <label htmlFor="file">Wybierz plik</label>
+                        <input type="file" id="file" onChange={uploadImage} />
+                    </div>
+                </Card>
+
+                <Card>
+                    <h3>Usuń zdjęcia</h3>
+                    <div className="dog-images">
+                        {dogImages &&
+                            dogImages.map((dogImage) => (
+                                <div className="dog-image-container">
+                                    <img
+                                        className="dog-image"
+                                        src={dogImage.fileUrl}
+                                        alt=""
+                                    />
+                                    <div>
+                                        <Button onClick={()=>removeFile(dogImage.fileId)}>Usuń zdjęcie</Button>
+                                    </div>
+                                </div>
+                            ))}
+                    </div>
+                </Card>
             </div>
         </BasicPage>
     )
--- routes/dogs/dog-gallery.js
+++ routes/dogs/dog-gallery.js
@@ -1,23 +1,64 @@
 const express = require('express')
 const multer = require('multer')
-const path = require("path");
+const path = require('path')
 const { provideFrontendData } = require('../../utils/provide-frontend-data')
 const router = express.Router()
-const crypto = require('crypto');
-const fs = require('fs');
-const {executeQuery} = require("../../utils/database-utils");
+const crypto = require('crypto')
+const fs = require('fs')
+const { executeQuery } = require('../../utils/database-utils')
 
+router.get('/dog-images/:dogId/:fileName', (req, res, next) => {
+    const { dogId, fileName } = req.params
 
-router.get('/photo-test',(req,res,next)=>{
-    res.sendFile(process.cwd() + '/userdata/test.jpg')
+    res.sendFile(process.cwd() + '/userdata/dogs/' + dogId + '/' + fileName)
+})
+
+router.delete('/API/dog/remove-image/:fileId', async (req, res, next) => {
+    const { fileId } = req.params
+    //TODO validation if user is owner of dog
+    const [image] = await executeQuery(
+        `SELECT uploaded_files.fileUrl FROM uploaded_files WHERE fileId = ?`,
+        [fileId]
+    )
+    await executeQuery(`DELETE FROM dog_images WHERE imageId = ?`, [fileId])
+
+    await executeQuery(`DELETE FROM uploaded_files WHERE fileId = ?`, [fileId])
+
+    fs.unlinkSync(`${process.cwd()}/${image.fileUrl}`)
+
+    res.send({ message: 'removed successfully' })
+})
+
+router.get('/API/dog/:dogId/images', async (req, res, next) => {
+    const { dogId } = req.params
+    const { userId } = req.signedCookies
+    if (!userId) {
+        return res.status(401).send({ message: 'Unauthorized' })
+    }
+
+    const images = await executeQuery(
+        `SELECT uploaded_files.fileName, uploaded_files.fileId, uploaded_files.uploadDatetime FROM dog_images 
+    INNER JOIN uploaded_files on uploaded_files.fileId = dog_images.imageId
+    WHERE dog_images.dogId = ?
+    ORDER BY uploaded_files.uploadDatetime
+    `,
+        [dogId]
+    )
+
+    const imagesWithFullUrl = images.map((image) => ({
+        ...image,
+        fileUrl: `/dog-images/${dogId}/${image.fileName}`,
+    }))
+
+    res.send(imagesWithFullUrl)
 })
 
 router.post('/API/dog/:dogId/add-photo', (req, res, next) => {
     const { dogId } = req.params
     const { userId } = req.signedCookies
-    const directory = 'userdata/dogs/'+dogId+'/'
-    if (!fs.existsSync(directory)){
-        fs.mkdirSync(directory);
+    const directory = 'userdata/dogs/' + dogId + '/'
+    if (!fs.existsSync(directory)) {
+        fs.mkdirSync(directory)
     }
 
     const storage = multer.diskStorage({
@@ -25,7 +66,10 @@ router.post('/API/dog/:dogId/add-photo', (req, res, next) => {
             cb(null, directory)
         },
         filename: function (req, file, cb) {
-            const hash = crypto.createHash('md5').update(file.fieldname+`${(new Date().getTime())}`).digest('hex');
+            const hash = crypto
+                .createHash('md5')
+                .update(file.fieldname + `${new Date().getTime()}`)
+                .digest('hex')
 
             cb(null, hash + '.jpg')
         },
@@ -57,18 +101,18 @@ router.post('/API/dog/:dogId/add-photo', (req, res, next) => {
         if (err) {
             res.send(err)
         } else {
-            const { destination, filename, size, path } =req.file
+            const { destination, filename, size, path } = req.file
             const { insertId } = await executeQuery(
-                `INSERT INTO uploaded_files (fileId, fileUrl, fileDir, fileName, ownerId, size) VALUES ( NULL, ?, ? ,? , ? , ? )`,
-                [path, destination, filename, userId, size ]
+                `INSERT INTO uploaded_files (fileId, fileUrl, fileDir, fileName, ownerId, size, uploadDatetime) VALUES ( NULL, ?, ? ,? , ? , ?, NOW())`,
+                [path, destination, filename, userId, size]
             )
 
             await executeQuery(
                 `INSERT INTO dog_images (dogImageId, dogId, imageId) VALUES ( NULL, ?, ?)`,
-                [dogId, insertId ]
+                [dogId, insertId]
             )
 
-            res.send({message: 'Success, Image uploaded!'})
+            res.send({ message: 'Success, Image uploaded!' })
         }
     })
 })
